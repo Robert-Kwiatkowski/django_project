28e61999e8e8aa1fd2fdceb3f29b1b4a6475b932
Robert <seken1233123@gmail.com>
2019-04-14 13:18:40 +0200

    Added some basics of the user interface

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from users import views as user_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('register/', user_views.register, name='register'),
@@ -27,3 +28,5 @@ urlpatterns = [
     url('', include('blog.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
