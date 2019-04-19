11ebe61e8354fa582308feaf707e5b9f4bcd6914
Robert <seken1233123@gmail.com>
2019-04-19 15:00:48 +0200

    Added required changes to deploy app on heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9au+vhd1=97@oawmt^93#pklo_)@748d1mt(fn^w%f&fe(!^bl'
-
+#SECRET_KEY = '9au+vhd1=97@oawmt^93#pklo_)@748d1mt(fn^w%f&fe(!^bl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['robertkwiatkowskiapp.herokuapp.com']
 
 
 # Application definition
